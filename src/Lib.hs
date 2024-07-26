b470153f599319beeb15332dcb4666d8017c556e
Ryley Higa <rhiga@sumologic.com>
2024-07-26 11:15:18 -1000

    Attempted to implement other estimation methods

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,13 +8,13 @@ import Text.Printf (printf)
 
 type Prob = Double
 
+-- | Define Discrete Distribution
+newtype Discrete a = Discrete {toList :: [(a, Prob)]}
+
 instance (Show a, Ord a) => Show (Discrete a) where
   show :: (Show a, Ord a) => Discrete a -> String
   show d = concatMap showRow $ toList d
     where showRow (elem, prob) = show elem ++ " (" ++ printf "%.4f" prob ++ ")\n"
-
--- | Represents categorical distributions only
-newtype Discrete a = Discrete {toList :: [(a, Prob)]}
 
 squish :: (Ord a) => Discrete a -> Discrete a
 squish (Discrete xs) = Discrete $ M.toList $ M.fromListWith (+) xs
@@ -40,17 +40,22 @@ sampleN g n d = sample g $ Control.Monad.replicateM n d
 
 -- | Want to add squish to fmap
 instance Functor Discrete where
+  fmap :: (a -> b) -> Discrete a -> Discrete b 
   fmap f (Discrete xs) = Discrete $ map (B.first f) xs
 
 instance Applicative Discrete where
+    pure :: a -> Discrete a
     pure x = Discrete [(x, 1)]
+    (<*>) :: Discrete (a -> b) -> Discrete a -> Discrete b
     (Discrete fs) <*> (Discrete xs) = Discrete $ do
         (f, p) <- fs
         (x, q) <- xs
         return (f x, p * q)
 
 instance Monad Discrete where
+    return :: a -> Discrete a
     return = pure
+    (>>=) :: Discrete a -> (a -> Discrete b) -> Discrete b
     (Discrete xs) >>= f = Discrete $ do
         (x, p) <- xs
         (y, q) <- toList $ f x
@@ -62,10 +67,19 @@ simpleEstimation :: R.StdGen -> Int -> Discrete Double -> Double
 simpleEstimation g n d = expectation $
     Discrete $ map (\x -> (x, 1 / fromIntegral n)) $ sampleN g n d
 
--- | Distributions
+-- | Discrete Distributions
 bern :: Double -> Discrete Int
 bern p = Discrete [(0, 1 - p), (1, p)]
 
 uniform :: [Int] -> Discrete Int
 uniform xs = normalize $ Discrete $ map (\x -> (x, 1)) xs
+
+
+-- | Define Continuous Distribution
+newtype Sampleable a = Samplable {runSample :: R.StdGen -> a}
+newtype Transform a b = Transform {runTransform :: a -> b}
+
+instance Functor (Transform b r) where
+  fmap :: (a -> b) -> Transform b r -> Transform a r 
+  fmap f (Transform g) = Transform  f . g 
 
